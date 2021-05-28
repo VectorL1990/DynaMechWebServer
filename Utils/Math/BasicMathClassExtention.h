02b84bd4989798ef67f29000bb21f9632a7e36ca
DMliangshiming <shimingliang@deepmirror.com>
2021-05-28 18:00:23 +0800

    add some matrix calculation

--- Utils/Math/BasicMathClassExtention.h
+++ Utils/Math/BasicMathClassExtention.h
@@ -12,6 +12,18 @@ struct Vec3d
     double X;
     double Y;
     double Z;
+
+    Vec3d(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    Vec3d operator-(Vec3d B)
+    {
+        return Vec3d(X-B.X, Y-B.Y, Z-B.Z); 
+    }
 };
 
 /**
--- Utils/Math/MathFunctionLibrary.h
+++ Utils/Math/MathFunctionLibrary.h
@@ -20,4 +20,20 @@ public:
     mat GetEigenVector(mat Matrix, float EigenValue);
 
     mat GetFinitRotationAxis(arma::mat TransposeMatrix);
+
+    arma::vec4 CalculateQuatByTransformMatrix(mat TransformMatrix);
+
+    mat GetRMatrixByQuat(vec4 Quat);
+
+    vec GetBMatrix(Vec3d P1, Vec3d P2, mat RMatrix);
+
+    mat GetSkewSymmetricMatrixByVec3(Vec3d Vec);
+
+    mat CalculateTransformMatrixDerivation(double dT, mat NextStepA, mat A);
+
+    mat CalculateAngularVelocity(mat NextStepA, mat A, double dT);
+
+    Vec3d GetAVVector(Vec3d AngularV, Vec3d P1, Vec3d P2);
+
+    mat GetDMatrix(Vec3d FixAVector, mat RMatrix);
 };
